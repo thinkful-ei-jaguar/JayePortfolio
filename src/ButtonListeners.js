f862d2cc0b9406dbfaf7592ac3d6cd1e79449558
Jaye Laguardia <jayelaguardia@gmail.com>
2019-12-30 23:46:59 -0500

    edits

--- src/ButtonListeners.js
+++ src/ButtonListeners.js
@@ -3,10 +3,10 @@ import $ from 'jquery';
 const generateHome = function () {
   return `
     <header role="banner">
-      <h1>Title</h1>
+      <h1>Lorem Ipsum</h1>
     </header>
     <section>
-      <h2>Headline</h2>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</h2>
     </section>
     <section>
       <button type="button" class="learnMore">Learn More</button>
@@ -17,13 +17,13 @@ const generateHome = function () {
 const generateLearnMore = function () {
   return `
     <header role="banner">
-      <h1>Title</h1>
+      <h1>Lorem Ipsum</h1>
     </header>
     <section>
-      <h2>Headline</h2>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</h2>
     </section>
     <section>
-      <p>About me</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum tempus egestas sed sed. Sit amet porttitor eget dolor morbi non arcu risus quis. Sagittis purus sit amet volutpat consequat mauris. At risus viverra adipiscing at in tellus integer feugiat. Et tortor at risus viverra. Non curabitur gravida arcu ac tortor dignissim. Urna et pharetra pharetra massa massa. Neque egestas congue quisque egestas diam in arcu cursus euismod. Ac odio tempor orci dapibus ultrices in iaculis nunc. Amet facilisis magna etiam tempor. Odio tempor orci dapibus ultrices in iaculis. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Et tortor at risus viverra. Bibendum est ultricies integer quis auctor elit sed. Id ornare arcu odio ut sem nulla pharetra diam sit. Id aliquet lectus proin nibh nisl condimentum id venenatis a. Amet commodo nulla facilisi nullam vehicula ipsum. Id faucibus nisl tincidunt eget nullam non nisi.</p>
     </section>
     <section>
       <button type="button" class="resume">Resume</button>
@@ -48,8 +48,13 @@ const generateResume = function () {
 
 const generateContactMe = function () {
   return `
+    <header role="banner">
+      <h1>Contact Me</h1>
+    </header>
     <section>
-      <p>Contact Me</p>
+      <a href="">My GitHub</a>
+      <a href="">My LinkedIn</a>
+      <p class = 'contact'> LoremIpsum@gmail.com </p>
     </section>
     <section>
       <button type="button" class="resume">Resume</button>
@@ -67,16 +72,20 @@ const generateSeeMyWork = function () {
 
     <section class = 'scrollmenu'>
       <div class = 'project'> 
-        <p>Project 1</p>
+        <p>Lorem Ipsum</p>
+        <div class = 'imgPlace'> <p>Img</p> </div>
+        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Technology: Lorem ipsum dolor</p>
+        <a href="">project repo</a>
+        <a href="">live app</a>
       </div>
       <div class = 'project'> 
-        <p>Project 2</p>
-      </div>
-      <div class = 'project'> 
-        <p>Project 3</p>
-      </div>
-      <div class = 'project'> 
-        <p>Project 4</p>
+        <p>Lorem Ipsum</p>
+        <div class = 'imgPlace'> <p>Img</p> </div>
+        <p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p>Technology: Lorem ipsum dolor</p>
+        <a href="">project repo</a>
+        <a href="">live app</a>
       </div>
     </section>
 
